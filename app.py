8dfe42803594ccc2d730dc5ef3065b18faf95a7a
rynatterson <126192950+rynatterson@users.noreply.github.com>
2024-01-17 10:32:28 -0800

    updates
    updated files in class

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -18,6 +18,28 @@ def products():
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+@app.route('/confirmation')
+def confirmation():
+    name = request.args.get("name")
+    phone = request.args.get("phn")
+    email = request.args.get("email")
+    add = request.args.get("add")
+    city = request.args.get("city")
+    state = request.args.get("state")
+    zip = request.args.get("zip")
+    print(name)
+    props = {
+        "name": name,
+        "phone": phone,
+        "email": email,
+        "address": add,
+        "city": city,
+        "state": state,
+        "zip": zip
+    }
+
+    return render_template("confirmation.html", data=props)
 
 if __name__ == '__main__':
     app.run(debug=True)
